574b90982ba99389cb572d89bd11c05dfcbd8899
Stevan Little <stevan.little@gmail.com>
2014-09-30 10:45:51 +0200

    actually, rework this one

--- lib/mop/internal/util.pm
+++ lib/mop/internal/util.pm
@@ -6,14 +6,34 @@ use experimental 'signatures', 'postderef';
 
 use Variable::Magic ();
 
-sub next_oid {
-    state $OID = 0;
-    $OID++;
+our $OID = 0;
+our $WIZ = Variable::Magic::wizard( data => sub { $_[1] } );
+
+sub next_oid { $OID++ }
+sub get_wiz  { $WIZ   }
+
+# TODO:
+#
+# 1) These assume that the instance 
+#    is a HASH ref, which is wrong,
+#    so we need to dispatch based on 
+#    ref type instead.
+# 
+# 2) There is not enough error handling
+#    here, we need to make sure that the
+#    call to getdata works, and die if
+#    the value does not have magic.
+# 
+# ...
+
+sub get_oid ($instance) { 
+    my $opaque = Variable::Magic::getdata( %$instance, $WIZ );
+    return $opaque->{id};
 }
 
-sub get_wiz {
-    state $WIZ = Variable::Magic::wizard( data => sub { $_[1] } );
-    $WIZ;
+sub get_slots ($instance) {
+    my $opaque = Variable::Magic::getdata( %$instance, $WIZ );
+    return $opaque->{slots};
 }
 
 1;
