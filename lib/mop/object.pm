e6201d4f4f92a225ef5b56b486445804881ab76b
Stevan Little <stevan.little@gmail.com>
2014-09-30 10:46:11 +0200

    add in mop::object

---
+++ lib/mop/object.pm
@@ -0,0 +1,25 @@
+package mop::object;
+
+use v5.20;
+use warnings;
+use experimental 'signatures', 'postderef';
+
+use mop::internal::util;
+
+use Variable::Magic ();
+
+sub new ($class, %args) {
+
+    my %self;
+
+    Variable::Magic::cast( %self, mop::internal::util::get_wiz(), {
+        id    => mop::internal::util::next_oid(),
+        slots => { %args }
+    });
+
+    bless \%self => $class;
+}
+
+1;
+
+__END__
