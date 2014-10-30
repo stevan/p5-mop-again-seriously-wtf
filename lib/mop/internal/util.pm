3afd459f105d3bb4eb8f50e361d9209177f436ea
Stevan Little <stevan.little@booking.com>
2014-10-30 16:02:13 +0100

    clean up mop::object

--- lib/mop/internal/util.pm
+++ lib/mop/internal/util.pm
@@ -4,8 +4,24 @@ use v5.20;
 use warnings;
 use experimental 'signatures', 'postderef';
 
+use Scalar::Util ();
+
+sub BUILDALL ($instance, $args) {
+    foreach my $c ( mro::get_linear_isa( Scalar::Util::blessed( $instance ) )->@* ) {
+        if ( my $build = $c->can('BUILD') ) {
+            $instance->$build( $args );
+        }
+    }
+    return;
 }
 
+sub DEMOLISHALL ($instance)  {
+    foreach my $c ( mro::get_linear_isa( Scalar::Util::blessed( $instance ) )->@* ) {
+        if ( my $demolish = $c->can('DEMOLISH') ) {
+            $instance->$demolish();
+        }
+    }
+    return;
 }
 
 1;
--- lib/mop/object.pm
+++ lib/mop/object.pm
@@ -5,30 +5,19 @@ use warnings;
 use experimental 'signatures', 'postderef';
 
 use mop::internal::finalize;
+use mop::internal::util;
 
 our $VERSION   = '0.01';
 our $AUTHORITY = 'cpan:STEVAN';
 
 sub new ($class, %args) {
-    $self->BUILDALL( \%args );
     my $self = mop::meta( $class )->construct_instance( \%args );
+    $self->can('BUILDALL') && mop::internal::util::BUILDALL( $self, \%args );
     $self;
 }
 
-sub BUILDALL ($self, $args) {
-    # ... TODO 
-
-    return $self;
-}
-
-sub DEMOLISHALL ($self, $args) {
-    # ... TODO
-
-    return $self;
-}
-
 sub DESTROY ($self) {
-    $self->can('DEMOLISH') && $self->DEMOLISHALL
+    $self->can('DEMOLISH') && mop::internal::util::DEMOLISHALL( $self )
 }
 
 1;
